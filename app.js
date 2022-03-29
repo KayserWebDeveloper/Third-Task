0dd565b7502557ec832f0661968bbbb917c1954d
kayser <kolarkaiser265@gmail.com>
2022-03-30 00:34:17 +0530

    eppa

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ mongoose.connect('mongodb://localhost:27017/thirdTask',{useNewUrlParser:true});
 const app = express();
 app.use("/", router);
 
-// Main Route 
-
-app.get("/",(req, res) => {
-    res.send("Hello world");
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
-})
+});
